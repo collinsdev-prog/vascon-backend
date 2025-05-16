bbc0693dff1dc16f82fb2ecb03f496f1f0caf512
collinsdev-prog <collinsngene@gmail.com>
2025-05-16 12:54:19 +0100

    REDIS_PUBLIC_URL UPDATE

--- src/config/redis.provider.ts
+++ src/config/redis.provider.ts
@@ -5,7 +5,7 @@ import { ConfigService } from './config.service';
 export const RedisProvider: Provider = {
   provide: 'REDIS_CLIENT',
   useFactory: (configService: ConfigService) => {
-    const redisUrl = configService.get('REDIS_URL');
+    const redisUrl = configService.get('REDIS_PUBLIC_URL');
     return new Redis(redisUrl);
   },
   inject: [ConfigService],
